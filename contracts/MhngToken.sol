2c1386e5d1a985fe9738f702bc2f6d87ea58cf37
minhhung123 <prohungnguyenminh@gmail.com>
2021-07-14 11:09:19 +0700

    Add transfer function

--- contracts/MhngToken.sol
+++ contracts/MhngToken.sol
@@ -1,16 +1,34 @@
 pragma solidity ^0.5.16;
 
 contract MhngToken {
+  string public name = "MHNG Token";
+  string public symbol = "MHNG";
+  string public standard = "MHNG Token v1.0";
   uint256 public totalSupply;
+
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+  );
+  
+  mapping(address => uint256) public balanceOf;
+  
 
   // Initialize the number of tokens available
   // Contructor
-  constructor() public {
-    totalSupply = 1000000;     // state variables
+  constructor(uint256 _initialSupply) public {
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;     // state variables
   }
   
+  // Transfer
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value);
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
 
-
-  // Set the number of token
-  // Read the number of token
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const MhngToken = artifacts.require("./MhngToken.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(MhngToken);
+  deployer.deploy(MhngToken, 1000000);
 };
